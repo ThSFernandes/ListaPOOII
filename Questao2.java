75931f2d22ef17dfc1baaf02a74506f2470b5005
Thiago Salles <112223120+ThSFernandes@users.noreply.github.com>
2023-12-31 14:46:51 -0300

    Atualização questao 2

--- Questao2.java
+++ Questao2.java
@@ -1,98 +1,78 @@
-// Interface Componente
-interface Renderable {
-    String render();
-    String text();
+interface Renderizavel {
+    String renderizar();
+    String texto();
 }
 
-// Componente Concreto
-class BasicWord implements Renderable {
-    private String content;
+class PalavraBasica implements Renderizavel {
+    private String conteudo;
 
-    public BasicWord(String content) {
-        this.content = content;
+    public PalavraBasica(String conteudo) {
+        this.conteudo = conteudo;
     }
 
-    public String render() {
-        return content;
+    public String renderizar() {
+        return conteudo;
     }
 
-    public String text() {
-        return content;
+    public String texto() {
+        return conteudo;
     }
 }
 
-// Decorador
-class TextDecorator implements Renderable {
-    protected Renderable component;
+class DecoradorTexto implements Renderizavel {
+    protected Renderizavel componente;
 
-    public TextDecorator(Renderable component) {
-        this.component = component;
+    public DecoradorTexto(Renderizavel componente) {
+        this.componente = componente;
     }
 
-    public String render() {
+    public String renderizar() {
         return "";
     }
 
-    public String text() {
+    public String texto() {
         return "";
     }
 }
 
-// Decoradores Concretos
-class BoldDecorator extends TextDecorator {
-    public BoldDecorator(Renderable component) {
-        super(component);
+class DecoradorNegrito extends DecoradorTexto {
+    public DecoradorNegrito(Renderizavel componente) {
+        super(componente);
     }
 
-    public String render() {
-        return "<b>" + component.render() + "</b>";
+    public String renderizar() {
+        return "<b>" + componente.renderizar() + "</b>";
     }
 
-    public String text() {
-        return "<b>" + component.text() + "</b>";
-    }
-}
-
-class ItalicDecorator extends TextDecorator {
-    public ItalicDecorator(Renderable component) {
-        super(component);
-    }
-
-    public String render() {
-        return "<i>" + component.render() + "</i>";
-    }
-
-    public String text() {
-        return "<i>" + component.text() + "</i>";
+    public String texto() {
+        return "<b>" + componente.texto() + "</b>";
     }
 }
 
-class UnderlineDecorator extends TextDecorator {
-    public UnderlineDecorator(Renderable component) {
-        super(component);
+class DecoradorItalico extends DecoradorTexto {
+    public DecoradorItalico(Renderizavel componente) {
+        super(componente);
     }
 
-    public String render() {
-        return "<u>" + component.render() + "</u>";
+    public String renderizar() {
+        return "<i>" + componente.renderizar() + "</i>";
     }
 
-    public String text() {
-        return "<u>" + component.text() + "</u>";
+    public String texto() {
+        return "<i>" + componente.texto() + "</i>";
     }
 }
 
-// Cliente
-public class Questao2 {
-    public static void main(String[] args) {
-        Renderable normalWord = new BasicWord("normal");
-        Renderable boldWord = new BoldDecorator(new BasicWord("negrito"));
-        Renderable italicWord = new ItalicDecorator(new BasicWord("itálico"));
-        Renderable underlineWord = new UnderlineDecorator(new BasicWord("sublinhado"));
+class DecoradorSublinhado extends DecoradorTexto {
+    public DecoradorSublinhado(Renderizavel componente) {
+        super(componente);
+    }
 
-        // Renderizando e exibindo as palavras
-        System.out.println(normalWord.render() + " " + normalWord.text());
-        System.out.println(boldWord.render() + " " + boldWord.text());
-        System.out.println(italicWord.render() + " " + italicWord.text());
-        System.out.println(underlineWord.render() + " " + underlineWord.text());
+    public String renderizar() {
+        return "<u>" + componente.renderizar() + "</u>";
+    }
+
+    public String texto() {
+        return "<u>" + componente.texto() + "</u>";
     }
 }
