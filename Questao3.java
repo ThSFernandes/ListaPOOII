61b781e9f1f09e612b9dc6802badf620b619560f
Thiago Salles <112223120+ThSFernandes@users.noreply.github.com>
2023-12-31 14:52:41 -0300

    Update Questao3.java

--- Questao3.java
+++ Questao3.java
@@ -1,78 +1,65 @@
-// Interface Component (Abstração do padrão Decorator)
-interface Attack {
-    void performAttack();
+
+interface Ataque {
+    void realizarAtaque();
 }
 
-// Concrete Component (Ataque Básico)
-class BasicAttack implements Attack {
+
+class AtaqueBasico implements Ataque {
     @Override
-    public void performAttack() {
+    public void realizarAtaque() {
         System.out.println("Realizando ataque básico!");
     }
 }
 
-// Decorator (Poder)
-abstract class PowerDecorator implements Attack {
-    protected Attack decoratedAttack;
 
-    public PowerDecorator(Attack decoratedAttack) {
-        this.decoratedAttack = decoratedAttack;
+abstract class DecoradorPoder implements Ataque {
+    protected Ataque ataqueDecorado;
+
+    public DecoradorPoder(Ataque ataqueDecorado) {
+        this.ataqueDecorado = ataqueDecorado;
     }
 
     @Override
-    public void performAttack() {
-        decoratedAttack.performAttack();
+    public void realizarAtaque() {
+        ataqueDecorado.realizarAtaque();
     }
 }
 
-// Concrete Decorators (Poderes Específicos)
-class Power1 extends PowerDecorator {
-    public Power1(Attack decoratedAttack) {
-        super(decoratedAttack);
+
+class Poder1 extends DecoradorPoder {
+    public Poder1(Ataque ataqueDecorado) {
+        super(ataqueDecorado);
     }
 
     @Override
-    public void performAttack() {
-        super.performAttack();
+    public void realizarAtaque() {
+        super.realizarAtaque();
         System.out.println("Adicionando poder 1 ao ataque!");
     }
 }
 
-class Power2 extends PowerDecorator {
-    public Power2(Attack decoratedAttack) {
-        super(decoratedAttack);
+class Poder2 extends DecoradorPoder {
+    public Poder2(Ataque ataqueDecorado) {
+        super(ataqueDecorado);
     }
 
     @Override
-    public void performAttack() {
-        super.performAttack();
+    public void realizarAtaque() {
+        super.realizarAtaque();
         System.out.println("Adicionando poder 2 ao ataque!");
     }
 }
 
-class Power3 extends PowerDecorator {
-    public Power3(Attack decoratedAttack) {
-        super(decoratedAttack);
+class Poder3 extends DecoradorPoder {
+    public Poder3(Ataque ataqueDecorado) {
+        super(ataqueDecorado);
     }
 
     @Override
-    public void performAttack() {
-        super.performAttack();
+    public void realizarAtaque() {
+        super.realizarAtaque();
         System.out.println("Adicionando poder 3 ao ataque!");
     }
 }
 
-// Cliente (Classe Principal)
-public class Questao3 {
-    public static void main(String[] args) {
-        // Criando o ataque básico
-        Attack basicAttack = new BasicAttack();
 
-        // Adicionando poderes ao ataque em tempo de execução
-        Attack poweredAttack = new Power3(new Power2(new Power1(basicAttack)));
-
-        // Simulando a execução do jogo
-        System.out.println("Executando ataque com poderes:");
-        poweredAttack.performAttack();
-    }
-}
